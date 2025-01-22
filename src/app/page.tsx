fd9e0c2355fe27fc589917a98e20a40998b9568f
SwaraliGijare <swaragijare99@gmail.com>
2025-01-22 11:32:30 +0530

    Header Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import ContactForm from '@/component/contactForm';
 import Footer from '@/component/footer';
+import Header from '@/component/header';
 
 export default function Page() {
   return (
     <div className="bg-gray-900">
       <ContactForm />
       <Footer />
+     <Header/>
     </div>
   );
 }
