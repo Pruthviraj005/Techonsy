1f32b63e3f14eb96b1faa85854f697dbb1b155df
Ashwini Patil <ashwinipatil100313@gmail.com>
2025-02-18 16:40:12 +0530

    design angular page

---
+++ src/app/technology/Angular/InfoCards.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react';
+
+const InfoCard = ({ title, description, icon }) => {
+  return (
+    <div className="flex flex-col items-center bg-white rounded-lg shadow-lg p-6 transition-all duration-300 hover:shadow-2xl">
+      <div className="text-blue-500">{icon}</div>
+      <h3 className="font-semibold text-lg mb-2 text-black">{title}</h3>
+      <p className="text-gray-700 text-center">{description}</p>
+    </div>
+  );
+};
+
+export default InfoCard;
