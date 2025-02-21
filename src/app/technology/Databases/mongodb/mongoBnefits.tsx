a75c03ba314955e4b023407626d608a76929b493
Pratiksha Joshi <pratujoshi18@gmail.com>
2025-02-21 11:32:00 +0530

    error resolved

--- src/app/technology/Databases/mongodb/mongoBnefits.tsx
+++ src/app/technology/Databases/mongodb/mongoBnefits.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect } from 'react';
 import { Cpu, Database, FileText, Share2, Users, Layers } from 'lucide-react';
 
--- src/app/technology/frontend/reactjs/Services.tsx
+++ src/app/technology/frontend/reactjs/Services.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 
 const ServiceCard = ({ icon, title, description, delay }) => (
